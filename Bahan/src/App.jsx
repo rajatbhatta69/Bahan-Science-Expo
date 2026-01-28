a2fa81b6a377bf9988e7b27dd3d7371a2b0f6eea
rajatbhatta69 <bhattarajat10@gmail.com>
2026-01-28 22:03:50 +0545

    Fixed the vercel analytics

--- Bahan/src/App.jsx
+++ Bahan/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 import MapDisplay from './components/MapDisplay';
 import DriverPortal from './pages/DriverPortal';
 import './App.css';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
